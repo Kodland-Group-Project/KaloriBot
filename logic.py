0c8bbe9018633c5f84990663e8d365ff942fdc90
Enderman9424 <ruzgar.cinar0601@gmail.com>
2025-11-26 22:07:16 +0300

    add logic

--- logic.py
+++ logic.py
@@ -12,4 +12,24 @@ class DBManager:
         return result[0] if result else None
     
     def create_table(self):
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS user(userid INTEGER PRIMARY KEY, username TEXT, )")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS user(userid INTEGER PRIMARY KEY, username TEXT, age INTEGER, height INTEGER, weight INTEGER, aim_of_calories_in_3_month INTEGER DEFAULT 0, totol_calories_in_3_month INTEGER DEFAULT 0)")
+
+    def add_user(self, userid, username, age=None, height=None, weight=None):
+        self.cursor.execute("INSERT INTO user (userid, username, age, height, weight) VALUES (?, ?, ?, ?, ?)", (userid, username, age, height, weight))
+        self.connection.commit()
+
+    def set_calories(self, userid, calories):
+        self.cursor.execute("UPDATE user SET aim_calories_in_3_month = ? WHERE userid = ?", (calories, userid))
+        self.connection.commit()
+
+    def add_calories(self, userid, calories):
+        self.cursor.execute("UPDATE user SET totol_calories_in_3_month = totol_calories_in_3_month + ? WHERE userid = ?", (calories, userid))
+        self.connection.commit()
+
+    def get_calories(self, userid):
+        self.cursor.execute("SELECT totol_calories_in_3_month FROM user WHERE userid = ?", (userid,))
+        result = self.cursor.fetchone()
+        return result[0] if result else None
+    
+def calori_calculating():
+    pass
