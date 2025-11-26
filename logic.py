e779b5db6b6cc98b11f4a0d0e08640b62e475f8a
Enderman9424 <ruzgar.cinar0601@gmail.com>
2025-11-26 22:27:30 +0300

    upgrade

--- logic.py
+++ logic.py
@@ -12,22 +12,22 @@ class DBManager:
         return result[0] if result else None
     
     def create_table(self):
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS user(userid INTEGER PRIMARY KEY, username TEXT, age INTEGER, height INTEGER, weight INTEGER, gender INTEGER, aim_of_calories INTEGER DEFAULT 0, totol_calories INTEGER DEFAULT 0)")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS user(userid INTEGER PRIMARY KEY, username TEXT, age INTEGER, height INTEGER, weight INTEGER, gender INTEGER, aim_of_calories INTEGER DEFAULT 0, total_calories INTEGER DEFAULT 0)")
 
     def add_user(self, userid, username, age=None, height=None, weight=None, gender=None):
         self.cursor.execute("INSERT INTO user (userid, username, age, height, weight) VALUES (?, ?, ?, ?, ?, ?)", (userid, username, age, height, weight, gender))
         self.connection.commit()
 
     def set_calories(self, userid, calories):
-        self.cursor.execute("UPDATE user SET aim_calories = ? WHERE userid = ?", (calories, userid))
+        self.cursor.execute("UPDATE user SET aim_of_calories = ? WHERE userid = ?", (calories, userid))
         self.connection.commit()
 
     def add_calories(self, userid, calories):
-        self.cursor.execute("UPDATE user SET totol_calories = totol_calories + ? WHERE userid = ?", (calories, userid))
+        self.cursor.execute("UPDATE user SET total_calories = total_calories + ? WHERE userid = ?", (calories, userid))
         self.connection.commit()
 
     def get_calories(self, userid):
-        self.cursor.execute("SELECT totol_calories FROM user WHERE userid = ?", (userid,))
+        self.cursor.execute("SELECT total_calories FROM user WHERE userid = ?", (userid,))
         result = self.cursor.fetchone()
         return result[0] if result else None
     
