de9bb750d11e0b167069db511a61c5908a886a48
Enderman9424 <ruzgar.cinar0601@gmail.com>
2025-11-26 21:51:29 +0300

    upgrade logic

--- logic.py
+++ logic.py
@@ -1,1 +1,15 @@
 import sqlite3
+
+class DBManager:
+    def __init__(self, db_name='app.db'):
+        self.connection = sqlite3.connect(db_name)
+        self.cursor = self.connection.cursor()
+        self.create_table()
+
+    def get_value(self, food_name):
+        self.cursor.execute("SELECT value FROM data WHERE Food_Items=?", (food_name,))
+        result = self.cursor.fetchone()
+        return result[0] if result else None
+    
+    def create_table(self):
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS user(userid INTEGER PRIMARY KEY, username TEXT, )")
