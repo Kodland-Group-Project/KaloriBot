eb67becd3f426e65bb9239b7f2a00762ca264ed2
Enderman9424 <ruzgar.cinar0601@gmail.com>
2025-11-26 22:21:24 +0300

    calculate system

--- logic.py
+++ logic.py
@@ -12,24 +12,29 @@ class DBManager:
         return result[0] if result else None
     
     def create_table(self):
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS user(userid INTEGER PRIMARY KEY, username TEXT, age INTEGER, height INTEGER, weight INTEGER, aim_of_calories_in_3_month INTEGER DEFAULT 0, totol_calories_in_3_month INTEGER DEFAULT 0)")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS user(userid INTEGER PRIMARY KEY, username TEXT, age INTEGER, height INTEGER, weight INTEGER, gender INTEGER, aim_of_calories INTEGER DEFAULT 0, totol_calories INTEGER DEFAULT 0)")
 
-    def add_user(self, userid, username, age=None, height=None, weight=None):
-        self.cursor.execute("INSERT INTO user (userid, username, age, height, weight) VALUES (?, ?, ?, ?, ?)", (userid, username, age, height, weight))
+    def add_user(self, userid, username, age=None, height=None, weight=None, gender=None):
+        self.cursor.execute("INSERT INTO user (userid, username, age, height, weight) VALUES (?, ?, ?, ?, ?, ?)", (userid, username, age, height, weight, gender))
         self.connection.commit()
 
     def set_calories(self, userid, calories):
-        self.cursor.execute("UPDATE user SET aim_calories_in_3_month = ? WHERE userid = ?", (calories, userid))
+        self.cursor.execute("UPDATE user SET aim_calories = ? WHERE userid = ?", (calories, userid))
         self.connection.commit()
 
     def add_calories(self, userid, calories):
-        self.cursor.execute("UPDATE user SET totol_calories_in_3_month = totol_calories_in_3_month + ? WHERE userid = ?", (calories, userid))
+        self.cursor.execute("UPDATE user SET totol_calories = totol_calories + ? WHERE userid = ?", (calories, userid))
         self.connection.commit()
 
     def get_calories(self, userid):
-        self.cursor.execute("SELECT totol_calories_in_3_month FROM user WHERE userid = ?", (userid,))
+        self.cursor.execute("SELECT totol_calories FROM user WHERE userid = ?", (userid,))
         result = self.cursor.fetchone()
         return result[0] if result else None
     
-def calori_calculating():
-    pass
+def calori_calculating(height, weight, age, gender):
+    if gender == 1:#woman
+        bmr = 655 + (9.6 * weight) + (1.8 * height) - (4.7 * age)
+        return bmr
+    elif gender == 2:#man
+        bmr = 66 + (13.7 * weight) + (5 * height) - (6.8 * age)
+        return bmr
